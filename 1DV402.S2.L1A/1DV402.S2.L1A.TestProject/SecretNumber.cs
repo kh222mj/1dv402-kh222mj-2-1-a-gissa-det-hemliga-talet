66afd68c48fd177dcf4d1194206391c96a2a6aaa
kh222mj <kh222mj@lnu.se>
2014-10-01 16:22:51 +0200

    uppgift redovisad

--- 1DV402.S2.L1A/1DV402.S2.L1A.TestProject/SecretNumber.cs
+++ 1DV402.S2.L1A/1DV402.S2.L1A.TestProject/SecretNumber.cs
@@ -10,52 +10,66 @@ namespace _1DV402.S2.L1A
     {
         private int _number;
 
-        private int _count; 
+        private int _count;
 
         public const int MaxNumberOfGuesses = 7;
-
+        public void Initialize()
+        {
+            _count = 0;          
+            Random myRandom = new Random();
+            _number = myRandom.Next(1, 101);
+        }
         public SecretNumber()
         {
             Initialize();
         }
-        public void Initialize()
-        {
-            int count = 0;
-            _count = count;          
-            Random myRandom = new Random();
-            int number = myRandom.Next(1, 101);
-            _number = number;
-        }
         public bool MakeGuess(int number) 
         {
-            number = _number;
-            _count++;
-            Console.WriteLine(string.Format("Gissning {0}:", _count));
-            number = int.Parse(Console.ReadLine()); 
-            if(number < _number)
+            if (_count > (MaxNumberOfGuesses - 1))
+            {
+                Initialize();
+                throw new ApplicationException();
+            }
+            else 
+            {
+                _count++;
+            }
+            if (number < 1)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if (number > 100)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if (number < _number)
             {
                 Console.WriteLine(string.Format("{0} är för lågt. Du har {1} gissningar kvar.", number, (MaxNumberOfGuesses - _count)));
+                return false;
             }
             if (number > _number)
             {
                 Console.WriteLine(string.Format("{0} är för högt. Du har {1} gissningar kvar.", number, (MaxNumberOfGuesses - _count)));
+                return false;
             }
             if (number == _number)
             {
                 Console.WriteLine(string.Format("RÄTT GISSAT. Du klarade det på {0} försök. ", number, (MaxNumberOfGuesses - _count)));
                 return true;
             }
-            if (_count > MaxNumberOfGuesses) 
+            if (_count > MaxNumberOfGuesses)
             {
                 if (number < _number)
                 {
                     Console.WriteLine(string.Format("{0} är för lågt. Du har {1} gissningar kvar.", number, (MaxNumberOfGuesses - _count)));
                     Console.WriteLine("Det hemliga talet är ", _number);
+                    return false;
                 }
                 if (number > _number)
                 {
                     Console.WriteLine(string.Format("{0} är för högt. Du har {1} gissningar kvar.", number, (MaxNumberOfGuesses - _count)));
                     Console.WriteLine("Det hemliga talet är ", _number);
+                    return false;
                 }
             }
             return false;
